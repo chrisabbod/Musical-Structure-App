aa64a31994fdd6055927198ed67a992f8b5b2194
Chris <chrisabbod@gmail.com>
2018-04-24 20:57:26 -0400

    <feat:> Add Constructor, Getters, and Setters to Audio Class

--- app/src/main/java/com/example/android/musicalstructureapp/Audio.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Audio.java
@@ -1,4 +1,27 @@
 package com.example.android.musicalstructureapp;
 
 public class Audio {
+    private String audioTitle;
+    private String artistName;
+
+    public Audio(String title, String artist){
+        audioTitle = title;
+        artistName = artist;
+    }
+
+    public String getAudioTitle() {
+        return audioTitle;
+    }
+
+    public void setAudioTitle(String audioTitle) {
+        this.audioTitle = audioTitle;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
 }
