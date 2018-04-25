df2685997556954ae0cad51901128809e4d0114c
Chris <chrisabbod@gmail.com>
2018-04-24 21:39:45 -0400

    <feat:> Add Adapter to MainActivity to Display List of Songs

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.musicalstructureapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -15,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Audio> audioArrayList = new ArrayList<Audio>();
 
         audioArrayList.add(new Audio("Stairway to Heaven", "Led Zeppelin"));
+        audioArrayList.add(new Audio("Born to Be Wild", "Steppenwolf"));
+        audioArrayList.add(new Audio("Hound Dog", "Elvis Presley"));
+        audioArrayList.add(new Audio("Somebody to Love", "Jefferson Airplane"));
+        audioArrayList.add(new Audio("What a Wonderful World", "Louis Armstrong"));
+        audioArrayList.add(new Audio("Pretty Woman", "Roy Orbison"));
+        audioArrayList.add(new Audio("Here Comes the Sun", "The Beatles"));
+        audioArrayList.add(new Audio("I Wanna Be Sedated", "Ramones"));
+        audioArrayList.add(new Audio("Hey Jude", "The Beatles"));
+        audioArrayList.add(new Audio("Blackbird", "The Beatles"));
+        audioArrayList.add(new Audio("(Don't Fear) The Reaper", "Blue Oyster Cult"));
+        audioArrayList.add(new Audio("Break On Through (to the Other Side", "The Doors"));
+        audioArrayList.add(new Audio("Dancing Days", "Led Zeppelin"));
+        audioArrayList.add(new Audio("Carpet Crawlers", "Genesis"));
+        audioArrayList.add(new Audio("Turn! Turn! Turn!", "The Byrds"));
+
+        AudioAdapter adapter = new AudioAdapter(this, audioArrayList);
+        ListView listView = (ListView)findViewById(R.id.audio_list);
+        listView.setAdapter(adapter);
     }
 }
