8cb1038d336f9aee481cc6936477b1bde336d507
Chris <chrisabbod@gmail.com>
2018-04-24 23:42:10 -0400

    <feat:> Display Audio Title and Artist Name in DetailActivity
    Pass intent extras from MainActivity to DetailActivity so audio title and artist name can be displayed

--- app/src/main/java/com/example/android/musicalstructureapp/DetailActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.musicalstructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String audioTitle = intent.getStringExtra("Audio Title");
+        String artistName = intent.getStringExtra("Artist Name");
+
+        TextView titleTextView = (TextView)findViewById(R.id.detail_title);
+        TextView artistTextView = (TextView)findViewById(R.id.detail_artist);
+
+        titleTextView.setText(audioTitle);
+        artistTextView.setText(artistName);
     }
 }
